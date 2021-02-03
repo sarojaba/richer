e3f5eab498885d1af7b9a32fa69e395cd598cf16
sarojaba <sarojaba@gmail.com>
2021-02-04 00:36:36 +0900

    Remove main function

--- richer/richer.py
+++ richer/richer.py
@@ -116,35 +116,3 @@ class PropertyRenderer:
             __table.add_row(style(f.name), cell(getattr(self.item, f.name)))
 
         return __table
-
-
-@dataclass
-class Inner:
-    id: int
-    name: str
-
-
-@dataclass
-class Row:
-    id: int
-    name: str
-    price: int
-    desc: str
-    now: datetime
-    inners: List[Inner]
-    inner: Inner
-
-
-if __name__ == '__main__':
-    from rich.console import Console
-
-    item0 = Row(9999, 'bear', 300,
-                'abcdeabcdeabcdeabcdeabcde', datetime.today(), [Inner(0, 'white'), Inner(1, 'black')], Inner(0, 'white'))
-    item1 = Row(1, 'apple', 200, '', datetime.today(),
-                [Inner(0, 'white'), Inner(1, 'black')], Inner(0, 'white'))
-
-    items = [item0, item1]
-
-    console = Console()
-    console.print(PropertyRenderer(item0))
-    console.print(ListRenderer(items))
